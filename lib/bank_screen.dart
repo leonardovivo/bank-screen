fe0d0716f61c58125cc40f4672359045337d6a1a
leonardovivo <leonardovivo3@gmail.com>
2024-06-24 02:11:44 -0300

    Renda e gastos adicionados na tela do banco e fontes alteradas

--- lib/bank_screen.dart
+++ lib/bank_screen.dart
@@ -1,3 +1,5 @@
+import 'package:bank_screen/income_box.dart';
+import 'package:bank_screen/spending_box.dart';
 import 'package:flutter/material.dart';
 
 class BankScreen extends StatelessWidget {
@@ -6,13 +8,14 @@ class BankScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 33, 28, 28),
+      backgroundColor: const Color.fromARGB(255, 36, 34, 34),
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 33, 28, 28),
+        backgroundColor: const Color.fromARGB(255, 36, 34, 34),
         title: RichText(
           text: const TextSpan(
             text: 'My Card',
             style: TextStyle(
+              fontFamily: 'NotoSans',
               color: Colors.white,
               fontWeight: FontWeight.bold,
               fontSize: 26,
@@ -22,60 +25,75 @@ class BankScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Container(
-          width: double.infinity,
-          height: 220,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            gradient: const LinearGradient(
-              colors: [
-                Color.fromARGB(255, 5, 45, 248),
-                Color.fromARGB(255, 250, 6, 250),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'VISA',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 26,
-                  ),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 220,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: const LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 5, 45, 248),
+                    Color.fromARGB(255, 250, 6, 250),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-                SizedBox(height: 20.0),
-                Text(
-                  'Balance',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
-                  ),
-                ),
-                Text(
-                  '\$7,630',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32,
-                  ),
-                ),
-                Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Text(
+                      'VISA',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Balance',
+                      style: TextStyle(
+                        fontFamily: 'NotoSans',
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      ),
+                    ),
+                    Text(
+                      '\$7,630',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                      ),
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '****8149',
+                              style: TextStyle(
+                                fontFamily: 'NotoSans',
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ],
+                        ),
                         Text(
-                          '****8149',
+                          '07/27',
                           style: TextStyle(
+                            fontFamily: 'NotoSans',
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                             fontSize: 17,
@@ -83,19 +101,26 @@ class BankScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Text(
-                      '07/27',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                      ),
-                    ),
                   ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IncomeBox(
+                  amount: '12,402',
+                  percentage: '4.5%',
+                ),
+                SizedBox(width: 20.0),
+                SpendingBox(
+                  amount: '8,392',
+                  percentage: '4.5%',
                 ),
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
