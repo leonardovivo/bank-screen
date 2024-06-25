5c9133494a11e95a3901121a7b1ff199b88bff97
leonardovivo <leonardovivo3@gmail.com>
2024-06-24 21:34:15 -0300

    Arquivo do CardBox e do QuickService incluídos e SingleChildScrollView adicionado

--- lib/bank_screen.dart
+++ lib/bank_screen.dart
@@ -1,5 +1,7 @@
+import 'package:bank_screen/card_box.dart';
 import 'package:bank_screen/income_box.dart';
 import 'package:bank_screen/spending_box.dart';
+import 'package:bank_screen/quick_service.dart';
 import 'package:flutter/material.dart';
 
 class BankScreen extends StatelessWidget {
@@ -23,104 +25,31 @@ class BankScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 220,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: const LinearGradient(
-                  colors: [
-                    Color.fromARGB(255, 5, 45, 248),
-                    Color.fromARGB(255, 250, 6, 250),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CardBox(),
+              SizedBox(height: 16.0),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IncomeBox(
+                    amount: '12,402',
+                    percentage: '4.5%',
+                  ),
+                  SizedBox(width: 20.0),
+                  SpendingBox(
+                    amount: '8,392',
+                    percentage: '4.5%',
+                  ),
+                ],
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'VISA',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 26,
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Text(
-                      'Balance',
-                      style: TextStyle(
-                        fontFamily: 'NotoSans',
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                      ),
-                    ),
-                    Text(
-                      '\$7,630',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32,
-                      ),
-                    ),
-                    Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '****8149',
-                              style: TextStyle(
-                                fontFamily: 'NotoSans',
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          '07/27',
-                          style: TextStyle(
-                            fontFamily: 'NotoSans',
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 17,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            const Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IncomeBox(
-                  amount: '12,402',
-                  percentage: '4.5%',
-                ),
-                SizedBox(width: 20.0),
-                SpendingBox(
-                  amount: '8,392',
-                  percentage: '4.5%',
-                ),
-              ],
-            ),
-          ],
+              SizedBox(height: 16.0),
+              QuickService(),
+            ],
+          ),
         ),
       ),
     );
